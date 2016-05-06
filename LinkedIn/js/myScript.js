94ece03f39fdda658b703645ad471b7519a2b3b3
lenguyenhoaiphu <lenguyenhoaiphu@gmail.com>
2016-05-06 10:01:40 +0700

    Step 2: Second Commit

--- LinkedIn/js/myScript.js
+++ LinkedIn/js/myScript.js
@@ -1,4 +1,8 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ["xeditable"]);
+app.run(function(editableOptions) {
+  editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+});
+
 app.controller('myController', function($scope, $http) {
   $http.get('data.json')
   .success(function(response) {
