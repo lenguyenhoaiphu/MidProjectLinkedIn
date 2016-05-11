24d7933ab215b4ec3c384573ffc9c69e2b80ecc0
lenguyenhoaiphu <lenguyenhoaiphu@gmail.com>
2016-05-11 15:15:06 +0700

    Step 3: Second Commit-FireBase

--- LinkedIn/js/myScript.js
+++ LinkedIn/js/myScript.js
@@ -1,13 +1,32 @@
-var app = angular.module('myApp', ["xeditable"]);
+var app = angular.module('myApp', ["xeditable", "firebase"]);
 app.run(function(editableOptions) {
   editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
 });
 
-app.controller('myController', function($scope, $http) {
-  $http.get('data.json')
-  .success(function(response) {
-      $scope.myData = response;
-  });
+app.controller('myController', function($scope, $firebaseObject, $firebaseArray) {
+	var ref= new Firebase("https://cvfirebase--hoaiphu.firebaseio.com/");
+  
+	var pro=ref.child("PROFILE");
+	$scope.profile=$firebaseObject(pro);
+	
+	var ima=ref.child("IMAGES");
+	$scope.images=$firebaseObject(ima);
+	
+	var sum=ref.child("SUMMARY");
+	$scope.summary=$firebaseArray(sum);
+	
+	var exp=ref.child("EXPERIENCE");
+	$scope.experience=$firebaseArray(exp);
+	
+	var pro=ref.child("PROJECT");
+	$scope.project=$firebaseArray(pro);
+	
+	var ski=ref.child("SKILLS");
+	$scope.skills=$firebaseArray(ski);
+	
+	var edu=ref.child("EDUCATION");
+	$scope.education=$firebaseArray(edu);
+	
   $scope.CancelFormSummary=function()
 	{
 		$scope.hidesummary=false;
@@ -32,31 +51,31 @@ app.controller('myController', function($scope, $http) {
 	$scope.AddSummary=function()
 	{
 		$scope.hidesummary=false;
-		$scope.myData.SUMMARY.push({key:$scope.summary});
-		$scope.summary="";
+		$scope.summary.push({key:$scope.textsummary});
+		$scope.textsummary="";s
 	};
 	$scope.AddExperience=function()
 	{
 		$scope.hideexperience=false;
-		$scope.myData.EXPERIENCE.push({key:$scope.experience});
-		$scope.experience="";
+		$scope.experience.push({key:$scope.textexperience});
+		$scope.textexperience="";
 	};
 	$scope.AddProject=function()
 	{
 		$scope.hideproject=false;
-		$scope.myData.PROJECT.push({key:$scope.project});
-		$scope.project="";
+		$scope.project.push({key:$scope.textproject});
+		$scope.textproject="";
 	};
 	$scope.AddSkill=function()
 	{
 		$scope.hideskills=false;
-		$scope.myData.SKILLS.push({key:$scope.skills});
-		$scope.skills="";
+		$scope.skills.push({key:$scope.textskills});
+		$scope.textskills="";
 	};
 	$scope.AddEducation=function()
 	{
 		$scope.hideeducation=false;
-		$scope.myData.EDUCATION.push({key:$scope.education});
-		$scope.education="";
+		$scope.education.push({key:$scope.texteducation});
+		$scope.texteducation="";
 	};
 });
