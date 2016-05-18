58f2f9867e9dd0978e47378dd404371e4de4fd61
lenguyenhoaiphu <lenguyenhoaiphu@gmail.com>
2016-05-18 16:33:59 +0700

    Step 4: Authentication

--- LinkedIn/js/myScript.js
+++ LinkedIn/js/myScript.js
@@ -3,8 +3,9 @@ app.run(function(editableOptions) {
   editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
 });
 
-app.controller('myController', function($scope, $firebaseObject, $firebaseArray) {
+app.controller('myController', function($scope, $firebaseObject, $firebaseArray, $firebaseAuth) {
 	var ref= new Firebase("https://cvfirebase--hoaiphu.firebaseio.com/");
+	$scope.authObj=$firebaseAuth(ref);
   
 	var pro=ref.child("PROFILE");
 	$scope.profile=$firebaseObject(pro);
@@ -26,6 +27,34 @@ app.controller('myController', function($scope, $firebaseObject, $firebaseArray)
 	
 	var edu=ref.child("EDUCATION");
 	$scope.education=$firebaseArray(edu);
+	
+	$scope.Login=function()//login
+    {
+        $scope.authObj.$authWithOAuthPopup("google").then(function(authData) {
+            console.log("Logged in as:", authData.uid);
+            $scope.login=true;
+        }).then(function() {
+            // Never called because of page redirect
+        }).catch(function(error) {
+            console.error("Authentication failed:", error);
+        });
+
+    };
+
+
+    $scope.Logout=function()//logout
+    {
+        $scope.authObj.$unauth();
+        $scope.authObj.$onAuth(function(authData) {
+            if (authData) {
+                console.log("Authenticated with uid:", authData.uid);
+            } else {
+                console.log("Client unauthenticated.")
+                $scope.login=false;
+            }
+        });
+
+    };
 	
   $scope.CancelFormSummary=function()
 	{
